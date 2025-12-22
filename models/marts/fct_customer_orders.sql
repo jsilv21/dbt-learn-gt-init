531ba1e92485717c37edc0fe13bd18b98f2e174b
John Silverwood <63758090+jsilv21@users.noreply.github.com>
2025-12-22 16:14:57 -0500

    more naming cleanups, moving ctes

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,26 +1,9 @@
 -- logical ctes
 ---- staging
-with customers as (
-    select
-        customer_id,
-        first_name || ' ' || last_name as full_name,
-        last_name as surname,
-        first_name as givenname
-    from {{ref('stg_jaffle_shop_customers')}}
-),
+with customers as ( select * from {{ref('stg_jaffle_shop_customers')}}),
+orders as (select * from {{ref('int_orders')}}),
 
-orders as (
-    select
-        row_number() over(partition by customer_id order by order_date, order_id) as user_order_seq,
-        order_id,
-        customer_id,
-        order_date,
-        valid_order_date,
-        status as order_status,
-        _etl_loaded_at
-    from {{ref('stg_jaffle_shop_orders')}}
-),
-
+-- can probably remove this 
 payments as (
     select
         payment_id,
@@ -34,32 +17,13 @@ payments as (
     where payment_status != 'fail'
 ),
 ----
-order_totals as (
-    select
-        order_id,
-        payment_status,
-        sum(payment_amount) as order_value_dollars
-    from payments
-    group by 1,2
-),
-
-order_values_joined as(
-    select
-        orders.*,
-        order_totals.payment_status,
-        order_totals.order_value_dollars
-    from orders
-    left join order_totals on orders.order_id = order_totals.order_id
-),
-
-
 --- marts
 customer_order_history as (
     select
         customers.customer_id,
         customers.full_name,
-        customers.surname,
-        customers.givenname,
+        customers.last_name,
+        customers.first_name,
         min(order_date) as first_order_date,
         min(valid_order_date) as first_non_returned_order_date,
         max(valid_order_date) as most_recent_non_returned_order_date,
@@ -72,7 +36,7 @@ customer_order_history as (
     join customers on orders.customer_id = customers.customer_id
     left outer join payments on orders.order_id = payments.order_id
     --where orders.order_status not in ('pending') (REMOVED AS NO DATA IN PENDING STATUS - BUT BE CAREFUL)
-    group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
+    group by customers.customer_id, customers.full_name, customers.last_name, customers.first_name
 ),
 
 -- final CTE
@@ -80,13 +44,13 @@ final as (
     select
     orders.order_id,
     orders.customer_id,
-    customers.surname,
-    customers.givenname,
+    customers.last_name,
+    customers.first_name,
     first_order_date,
     order_count,
     total_lifetime_value,
     payments.payment_amount as order_value_dollars, --need to be fixed as will agg multiple payments
-    orders.order_status,
+    orders.status as order_status,
     payments.payment_status
 from orders
 join customers on orders.customer_id = customers.customer_id
--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -1,8 +1,9 @@
     select
+        row_number() over(partition by user_id order by order_date, id) as user_order_seq,
         id as order_id,
         user_id as customer_id,
         order_date,
-        case when status not in ('returned', 'return_pending') then order_date end as valid_order_date, 
+        case when status not in ('returned', 'return_pending') then order_date else null end as valid_order_date, 
         status,
         _etl_loaded_at
 from {{source('jaffle_shop', 'orders')}}
