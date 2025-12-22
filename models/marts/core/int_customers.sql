7654f644d8493dc37f0138316914d6633132d6f3
John Silverwood <63758090+jsilv21@users.noreply.github.com>
2025-12-22 17:01:39 -0500

    pulling out int_customers, need to fix div by zero issue and perf final audit

---
+++ models/marts/core/int_customers.sql
@@ -0,0 +1,31 @@
+-- imports
+with customers as ( select * from {{ref('stg_jaffle_shop_customers')}}),
+orders as (select * from {{ref('int_orders')}}),
+
+--
+customer_orders as (
+    select 
+        orders.*,
+        customers.full_name,
+        customers.last_name,
+        customers.first_name,
+        -- cust level aggs
+        min(order_date) over (partition by orders.customer_id) as customer_first_order_date,
+        min(valid_order_date) over (partition by orders.customer_id) as customer_first_non_returned_order_date,
+        max(valid_order_date) over (partition by orders.customer_id) as customer_most_recent_non_returned_order_date,
+        count(*) over (partition by orders.customer_id) as customer_order_count,
+        sum(nvl2(valid_order_date,1,0)) over (partition by orders.customer_id) as customer_non_returned_order_count,
+        sum(nvl2(valid_order_date,order_value_dollars,0)) over (partition by orders.customer_id) as customer_total_lifetime_value,
+        array_agg(distinct orders.order_id) over (partition by orders.customer_id) as customer_order_ids
+    from orders
+    inner join customers on orders.customer_id = customers.customer_id
+),
+
+customer_average_order_value as (select 
+    *,
+    --TODO: fix safe div by zero
+    customer_total_lifetime_value/customer_non_returned_order_count as customer_avg_non_returned_order_value --! fix safe div by zero
+    from customer_orders
+)
+
+select * from customer_average_order_value
--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,33 +1,6 @@
--- imports
-with customers as ( select * from {{ref('stg_jaffle_shop_customers')}}),
-orders as (select * from {{ref('int_orders')}}),
-
-
---
-customer_orders as (
-    select 
-        orders.*,
-        customers.full_name,
-        customers.last_name,
-        customers.first_name,
-        -- cust level aggs
-        min(order_date) over (partition by orders.customer_id) as customer_first_order_date,
-        min(valid_order_date) over (partition by orders.customer_id) as customer_first_non_returned_order_date,
-        max(valid_order_date) over (partition by orders.customer_id) as customer_most_recent_non_returned_order_date,
-        count(*) over (partition by orders.customer_id) as customer_order_count,
-        sum(nvl2(valid_order_date,1,0)) over (partition by orders.customer_id) as customer_non_returned_order_count,
-        sum(nvl2(valid_order_date,order_value_dollars,0)) over (partition by orders.customer_id) as customer_total_lifetime_value,
-        array_agg(distinct orders.order_id) over (partition by orders.customer_id) as customer_order_ids
-    from orders
-    inner join customers on orders.customer_id = customers.customer_id
+with customer_orders as (
+    select * from {{ref('int_customers')}}
 ),
-
-customer_average_order_value as (select 
-    *,
-    customer_total_lifetime_value/customer_non_returned_order_count as customer_avg_non_returned_order_value
-    from customer_orders
-),
-
 --- marts
 
 -- final CTE
@@ -43,7 +16,7 @@ final as (
     order_value_dollars, --need to be fixed as will agg multiple payments
     status as order_status,
     payment_status
-from customer_average_order_value
+from customer_orders
 )
 
 -- final select
