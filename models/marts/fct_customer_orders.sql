66b78d7e58d472ddca1bce222fbd8ac262ef31de
John Silverwood <63758090+jsilv21@users.noreply.github.com>
2025-12-22 15:48:56 -0500

    centralizing case when logic

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -15,6 +15,7 @@ orders as (
         order_id,
         customer_id,
         order_date,
+        valid_order_date,
         status as order_status,
         _etl_loaded_at
     from {{ref('stg_jaffle_shop_orders')}}
@@ -30,7 +31,28 @@ payments as (
         _batched_at,
         created_at as payment_created_at
     from {{ref('stg_stripe_payments')}}
+    where payment_status != 'fail'
 ),
+----
+order_totals as (
+    select
+        order_id,
+        payment_status,
+        sum(payment_amount) as order_value_dollars
+    from payments
+    group by 1,2
+),
+
+order_values_joined as(
+    select
+        orders.*,
+        order_totals.payment_status,
+        order_totals.order_value_dollars
+    from orders
+    left join order_totals on orders.order_id = order_totals.order_id
+),
+
+
 --- marts
 customer_order_history as (
     select
@@ -39,17 +61,17 @@ customer_order_history as (
         customers.surname,
         customers.givenname,
         min(order_date) as first_order_date,
-        min(case when orders.order_status not in ('returned', 'return_pending') then order_date end) as first_non_returned_order_date,
-        max(case when orders.order_status not in ('returned', 'return_pending') then order_date end) as most_recent_non_returned_order_date,
+        min(valid_order_date) as first_non_returned_order_date,
+        max(valid_order_date) as most_recent_non_returned_order_date,
         coalesce(max(user_order_seq),0) as order_count,
-        coalesce(count(case when orders.order_status != 'returned' then 1 end),0 ) as non_returned_order_count,
-        sum(case when orders.order_status not in ('returned', 'return_pending') then payments.payment_amount else 0 end) as total_lifetime_value,
-        sum(case when orders.order_status not in ('returned', 'return_pending') then payments.payment_amount else 0 end)/nullif(count(case when orders.order_status not in ('returned', 'return_pending') then 1 end),0) as avg_non_returned_order_value,
+        coalesce(count(case when orders.valid_order_date is not null then 1 end),0 ) as non_returned_order_count,
+        sum(case when orders.valid_order_date is not null then payments.payment_amount else 0 end) as total_lifetime_value,
+        sum(case when orders.valid_order_date is not null then payments.payment_amount else 0 end)/nullif(count(case when orders.valid_order_date is not null then 1 end),0) as avg_non_returned_order_value,
         array_agg(distinct orders.order_id) as order_ids
     from orders
     join customers on orders.customer_id = customers.customer_id
     left outer join payments on orders.order_id = payments.order_id
-    where orders.order_status not in ('pending') and payments.payment_status != 'fail'
+    --where orders.order_status not in ('pending') (REMOVED AS NO DATA IN PENDING STATUS - BUT BE CAREFUL)
     group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 ),
 
@@ -70,7 +92,6 @@ from orders
 join customers on orders.customer_id = customers.customer_id
 join customer_order_history on orders.customer_id = customer_order_history.customer_id
 left outer join payments on orders.order_id = payments.order_id
-where payments.payment_status != 'fail'
 
 )
 
--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -2,6 +2,7 @@
         id as order_id,
         user_id as customer_id,
         order_date,
+        case when status not in ('returned', 'return_pending') then order_date end as valid_order_date, 
         status,
         _etl_loaded_at
 from {{source('jaffle_shop', 'orders')}}
