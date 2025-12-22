fda2d5022bf00898bdef288e6c463e157d9918a5
John Silverwood <63758090+jsilv21@users.noreply.github.com>
2025-12-22 16:48:15 -0500

    simplifying customer_orders & final cte

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -2,43 +2,48 @@
 with customers as ( select * from {{ref('stg_jaffle_shop_customers')}}),
 orders as (select * from {{ref('int_orders')}}),
 
---- marts
-customer_order_history as (
-    select
-        customers.customer_id,
+
+--
+customer_orders as (
+    select 
+        orders.*,
         customers.full_name,
         customers.last_name,
         customers.first_name,
-        min(order_date) as first_order_date,
-        min(valid_order_date) as first_non_returned_order_date,
-        max(valid_order_date) as most_recent_non_returned_order_date,
-        coalesce(max(user_order_seq),0) as order_count,
-        coalesce(count(case when orders.valid_order_date is not null then 1 end),0 ) as non_returned_order_count,
-        sum(case when orders.valid_order_date is not null then orders.order_value_dollars else 0 end) as total_lifetime_value,
-        sum(case when orders.valid_order_date is not null then orders.order_value_dollars else 0 end)/nullif(count(case when orders.valid_order_date is not null then 1 end),0) as avg_non_returned_order_value,
-        array_agg(distinct orders.order_id) as order_ids
+        -- cust level aggs
+        min(order_date) over (partition by orders.customer_id) as customer_first_order_date,
+        min(valid_order_date) over (partition by orders.customer_id) as customer_first_non_returned_order_date,
+        max(valid_order_date) over (partition by orders.customer_id) as customer_most_recent_non_returned_order_date,
+        count(*) over (partition by orders.customer_id) as customer_order_count,
+        sum(nvl2(valid_order_date,1,0)) over (partition by orders.customer_id) as customer_non_returned_order_count,
+        sum(nvl2(valid_order_date,order_value_dollars,0)) over (partition by orders.customer_id) as customer_total_lifetime_value,
+        array_agg(distinct orders.order_id) over (partition by orders.customer_id) as customer_order_ids
     from orders
-    join customers on orders.customer_id = customers.customer_id
-    --where orders.order_status not in ('pending') (REMOVED AS NO DATA IN PENDING STATUS - BUT BE CAREFUL)
-    group by customers.customer_id, customers.full_name, customers.last_name, customers.first_name
+    inner join customers on orders.customer_id = customers.customer_id
 ),
+
+customer_average_order_value as (select 
+    *,
+    customer_total_lifetime_value/customer_non_returned_order_count as customer_avg_non_returned_order_value
+    from customer_orders
+),
+
+--- marts
 
 -- final CTE
 final as (
     select
-    orders.order_id,
-    orders.customer_id,
-    customers.last_name,
-    customers.first_name,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    orders.order_value_dollars, --need to be fixed as will agg multiple payments
-    orders.status as order_status,
-    orders.payment_status
-from orders
-join customers on orders.customer_id = customers.customer_id
-join customer_order_history on orders.customer_id = customer_order_history.customer_id
+    order_id,
+    customer_id,
+    last_name,
+    first_name,
+    customer_first_order_date as first_order_date,
+    customer_order_count as order_count,
+    customer_total_lifetime_value as total_lifetime_value,
+    order_value_dollars, --need to be fixed as will agg multiple payments
+    status as order_status,
+    payment_status
+from customer_average_order_value
 )
 
 -- final select
